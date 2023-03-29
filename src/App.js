8b93c6b34f78de8e2070f364229dccbe4c5cb51a
Alpas Shrivastava <alpasshrivastava167@gmail.com>
2023-03-29 20:17:15 +0530

    29march

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import { Home } from './Frontend/Pages/Home/Home';
-import Header from './Frontend/Components/Header';
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./Frontend/Pages/Home/Home";
+import Header from "./Frontend/Components/Header";
+
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Header />}>
-          <Route exact path='/' element={<Home />} />
+          <Route exact path="/" element={<Home />} />
         </Route>
       </Routes>
     </>
--- src/Frontend/Pages/Home/Home.jsx
+++ src/Frontend/Pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Marquee from "react-fast-marquee";
 
+
 export const Home = () => {
   const [handleDropdownMenu, setHandleDropdownMenu] = useState(false);
 
@@ -97,7 +98,7 @@ export const Home = () => {
           <div className="desh_div">
             <img src="https://acharan.in/img/myapp.jpg" alt="" />
           </div>
-          <h2></h2>
+          <h2 className="text-center mt-2">देश</h2>
 
           <div className="row mt-4">
             <div className="col-sm-4">
@@ -147,28 +148,246 @@ export const Home = () => {
               <p>देश: 150 देशों की खुशहाली सूची में भारत 136 वें नंबर पर</p>
             </div>
           </div>
+          <div className="row mt-4">
+            <div className="col-sm-4">
+              <img
+                className="col_Img"
+                src="https://acharan.in/Admin_panel/public/image/news_image/1679404578.jpg"
+                alt=""
+              />
+            </div>
+            <div className="col-sm-8 ">
+              <p>देश: 150 देशों की खुशहाली सूची में भारत 136 वें नंबर पर</p>
+            </div>
+          </div>
+          <div className="antarastriya mt-4 ">
+            <div className="card_icon text-center">
+              <i class="fa-solid fa-sort-down"></i>
+            </div>
+            <h3 className="text-center antarastriya_h3 ">अंतरराष्ट्रीय</h3>
+            <div className="antarastriya_div">
+              <img
+                src="https://acharan.in/Admin_panel/public/image/news_image/1679902674.jpg"
+                alt=""
+              />
+              <span>
+                वैज्ञानिकों को संकेत मिले हैं कि धरती का अंत कैसे होगा?
+              </span>
+            </div>
+            <div className="antarastriya_div">
+              <img
+                src="https://acharan.in/Admin_panel/public/image/news_image/1679902674.jpg"
+                alt=""
+              />
+              <span>
+                वैज्ञानिकों को संकेत मिले हैं कि धरती का अंत कैसे होगा?
+              </span>
+            </div>
+            <div className="antarastriya_div">
+              <img
+                src="https://acharan.in/Admin_panel/public/image/news_image/1679902674.jpg"
+                alt=""
+              />
+              <span>
+                वैज्ञानिकों को संकेत मिले हैं कि धरती का अंत कैसे होगा?
+              </span>
+            </div>
+            <div className="antarastriya_div">
+              <img
+                src="https://acharan.in/Admin_panel/public/image/news_image/1679902674.jpg"
+                alt=""
+              />
+              <span>
+                वैज्ञानिकों को संकेत मिले हैं कि धरती का अंत कैसे होगा?
+              </span>
+            </div>
+            <div className="antarastriya_div">
+              <img
+                src="https://acharan.in/Admin_panel/public/image/news_image/1679902674.jpg"
+                alt=""
+              />
+              <span>
+                वैज्ञानिकों को संकेत मिले हैं कि धरती का अंत कैसे होगा?
+              </span>
+            </div>
+          </div>
         </div>
         <div className="col-sm-6">
-          <div className="card_div text-center">
-            <div className="card_icon">
+          <div className="card_div ">
+            <div className="card_icon text-center">
               <i class="fa-solid fa-sort-down"></i>
             </div>
             <h5 className="rajniti_h5">राजनीति</h5>
-            <p className="rajniti_text">
+            <p className="rajniti_text text-center">
               <span className="rajniti_spl">राजनीति -</span> ममता की नवीन पटनायक
               से मुलाकात, तीसरे मोर्चा को लेकर बनेगी बात
             </p>
-            <img
-              src="https://acharan.in/Admin_panel/public/image/news_image/1679576169.jpg"
-              alt=""
-            />
+            <center>
+              <img
+                src="https://acharan.in/Admin_panel/public/image/news_image/1679576169.jpg"
+                alt=""
+                className="w-100"
+              />
+            </center>
+            <div className="rajniti_below_flex mt-3 fs-5">
+              <span className="fw-bold">राजनीति</span>
+              <div className="d-flex gap-18px">
+                <i className="fa-brands fa-facebook text-secondary"></i>
+                <i className="fa-brands fa-instagram text-secondary"></i>
+                <i className="fa-brands fa-twitter text-secondary"></i>
+              </div>
+            </div>
+          </div>
+          <div className="row rajniti_below_div mt-4 mb-2">
+            <div className="col-sm-4">
+              <img
+                src="https://acharan.in/Admin_panel/public/image/news_image/1679749886.jpg"
+                alt=""
+              />
+            </div>
+            <div className="col-sm-8">
+              <span>वायनाड : अब सबकी नजरें चुनाव आयोग पर</span>
+            </div>
+          </div>
+          <div className="row rajniti_below_div mt-4 mb-2">
+            <div className="col-sm-4">
+              <img
+                src="https://acharan.in/Admin_panel/public/image/news_image/1679749886.jpg"
+                alt=""
+              />
+            </div>
+            <div className="col-sm-8">
+              <span>वायनाड : अब सबकी नजरें चुनाव आयोग पर</span>
+            </div>
+          </div>
+          <div className="row rajniti_below_div mt-4 mb-2">
+            <div className="col-sm-4">
+              <img
+                src="https://acharan.in/Admin_panel/public/image/news_image/1679749886.jpg"
+                alt=""
+              />
+            </div>
+            <div className="col-sm-8">
+              <span>वायनाड : अब सबकी नजरें चुनाव आयोग पर</span>
+            </div>
+          </div>
+          <div className="row rajniti_below_div mt-4 mb-2">
+            <div className="col-sm-4">
+              <img
+                src="https://acharan.in/Admin_panel/public/image/news_image/1679749886.jpg"
+                alt=""
+              />
+            </div>
+            <div className="col-sm-8">
+              <span>वायनाड : अब सबकी नजरें चुनाव आयोग पर</span>
+            </div>
+          </div>
+          <div className="row rajniti_below_div mt-4 mb-2">
+            <div className="col-sm-4">
+              <img
+                src="https://acharan.in/Admin_panel/public/image/news_image/1679749886.jpg"
+                alt=""
+              />
+            </div>
+            <div className="col-sm-8">
+              <span>वायनाड : अब सबकी नजरें चुनाव आयोग पर</span>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-12">
+              <div className="d-flex flex-nowrap justify-content-around h-50px border-bottom-5px-red">
+                <span className="text-danger fw-bold p-2">health</span>
+                <span className="text-danger fw-bold p-2">health</span>
+                <span className="text-danger fw-bold p-2">health</span>
+              </div>
+              <div className="homePageScrollDiv">
+                <div className="row mb-2">
+                  <div className="col-md-4">
+                  <img
+                    src="https://acharan.in/Admin_panel/public/image/news_image/1679749886.jpg"
+                    className="w-100"
+                    alt=""
+                  />
+                  </div>
+                  <div className="col-md-8">
+                    <span>हद से ज्यादा जिम आपकी जान को खतरे में डाल सकता हैं : कार्डियोलॉजिस्ट</span><br />
+                    <small>2023-03-29 11:54:21</small>
+                  </div>
+                </div>
+                <div className="row mb-2">
+                  <div className="col-md-4">
+                  <img
+                    src="https://acharan.in/Admin_panel/public/image/news_image/1679749886.jpg"
+                    className="w-100"
+                    alt=""
+                  />
+                  </div>
+                  <div className="col-md-8">
+                    <span>हद से ज्यादा जिम आपकी जान को खतरे में डाल सकता हैं : कार्डियोलॉजिस्ट</span><br />
+                    <small>2023-03-29 11:54:21</small>
+                  </div>
+                </div>
+                <div className="row mb-2">
+                  <div className="col-md-4">
+                  <img
+                    src="https://acharan.in/Admin_panel/public/image/news_image/1679749886.jpg"
+                    className="w-100"
+                    alt=""
+                  />
+                  </div>
+                  <div className="col-md-8">
+                    <span>हद से ज्यादा जिम आपकी जान को खतरे में डाल सकता हैं : कार्डियोलॉजिस्ट</span><br />
+                    <small>2023-03-29 11:54:21</small>
+                  </div>
+                </div>
+                <div className="row mb-2">
+                  <div className="col-md-4">
+                  <img
+                    src="https://acharan.in/Admin_panel/public/image/news_image/1679749886.jpg"
+                    className="w-100"
+                    alt=""
+                  />
+                  </div>
+                  <div className="col-md-8">
+                    <span>हद से ज्यादा जिम आपकी जान को खतरे में डाल सकता हैं : कार्डियोलॉजिस्ट</span><br />
+                    <small>2023-03-29 11:54:21</small>
+                  </div>
+                </div>
+                <div className="row mb-2">
+                  <div className="col-md-4">
+                  <img
+                    src="https://acharan.in/Admin_panel/public/image/news_image/1679749886.jpg"
+                    className="w-100"
+                    alt=""
+                  />
+                  </div>
+                  <div className="col-md-8">
+                    <span>हद से ज्यादा जिम आपकी जान को खतरे में डाल सकता हैं : कार्डियोलॉजिस्ट</span><br />
+                    <small>2023-03-29 11:54:21</small>
+                  </div>
+                </div>
+                <div className="row mb-2">
+                  <div className="col-md-4">
+                  <img
+                    src="https://acharan.in/Admin_panel/public/image/news_image/1679749886.jpg"
+                    className="w-100"
+                    alt=""
+                  />
+                  </div>
+                  <div className="col-md-8">
+                    <span>हद से ज्यादा जिम आपकी जान को खतरे में डाल सकता हैं : कार्डियोलॉजिस्ट</span><br />
+                    <small>2023-03-29 11:54:21</small>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
         <div className="col-sm-3">
           <div className="desh_div">
             <img src="https://acharan.in/img/myapp.jpg" alt="" />
           </div>
-          <h2></h2>
+          <h2 className="text-center mt-2">देश</h2>
           <div className="row mt-4">
             <div className="col-sm-4 col_Img">
               <img
@@ -215,6 +434,69 @@ export const Home = () => {
             </div>
             <div className="col-sm-8 ">
               <p>देश: 150 देशों की खुशहाली सूची में भारत 136 वें नंबर पर</p>
+            </div>
+          </div>
+          <div className="row mt-4">
+            <div className="col-sm-4">
+              <img
+                className="col_Img"
+                src="https://acharan.in/Admin_panel/public/image/news_image/1679404578.jpg"
+                alt=""
+              />
+            </div>
+            <div className="col-sm-8 ">
+              <p>देश: 150 देशों की खुशहाली सूची में भारत 136 वें नंबर पर</p>
+            </div>
+          </div>
+          <div className="antarastriya mt-4 ">
+            <div className="card_icon text-center">
+              <i class="fa-solid fa-sort-down"></i>
+            </div>
+            <h3 className="text-center antarastriya_h3">क्रिकेट अपडेट</h3>
+            <div className="antarastriya_div">
+              <img
+                src="https://acharan.in/Admin_panel/public/image/news_image/1679902674.jpg"
+                alt=""
+              />
+              <span>
+                वैज्ञानिकों को संकेत मिले हैं कि धरती का अंत कैसे होगा?
+              </span>
+            </div>
+            <div className="antarastriya_div">
+              <img
+                src="https://acharan.in/Admin_panel/public/image/news_image/1679902674.jpg"
+                alt=""
+              />
+              <span>
+                वैज्ञानिकों को संकेत मिले हैं कि धरती का अंत कैसे होगा?
+              </span>
+            </div>
+            <div className="antarastriya_div">
+              <img
+                src="https://acharan.in/Admin_panel/public/image/news_image/1679902674.jpg"
+                alt=""
+              />
+              <span>
+                वैज्ञानिकों को संकेत मिले हैं कि धरती का अंत कैसे होगा?
+              </span>
+            </div>
+            <div className="antarastriya_div">
+              <img
+                src="https://acharan.in/Admin_panel/public/image/news_image/1679902674.jpg"
+                alt=""
+              />
+              <span>
+                वैज्ञानिकों को संकेत मिले हैं कि धरती का अंत कैसे होगा?
+              </span>
+            </div>
+            <div className="antarastriya_div">
+              <img
+                src="https://acharan.in/Admin_panel/public/image/news_image/1679902674.jpg"
+                alt=""
+              />
+              <span>
+                वैज्ञानिकों को संकेत मिले हैं कि धरती का अंत कैसे होगा?
+              </span>
             </div>
           </div>
         </div>
